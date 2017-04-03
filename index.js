e3b9b66640f6616ca0599af41fbbde9faa0f0988
Chad Hietala <chadhietala@gmail.com>
2017-04-03 11:05:49 -0700

    Why did I loose these changes

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ function stripGlimmerUtils(babel) {
     name: 'strip glimmer utils',
     visitor: {
       Program(path, state) {
-        let unwrap = path.scope.getBinding('unwrap');
-        if (unwrap.kind === 'module' && unwrap.path.parentPath.node.source.value === state.opts.source) {
-          unwrap.referencePaths.forEach(path => {
-            if (path.parentPath.isCallExpression()) {
-              path.parentPath.replaceWith(path.parentPath.node.arguments[0]);
+        let source = state.opts.source;
+        state.opts.bindings.forEach((binding) => {
+          let b = path.scope.getBinding(binding);
+          if (b && b.kind === 'module' && b.path.parentPath.node.source.value === source) {
+            b.referencePaths.forEach(path => {
+              if (path.parentPath.isCallExpression()) {
+                path.parentPath.replaceWith(path.parentPath.node.arguments[0]);
+              }
+              b.path.remove();
+            });
+          }
+        });
+
+        let body = path.get('body');
+
+        for (let i = 0; i < body.length; i++) {
+          let decl = body[i];
+          let specifiers = decl.node.specifiers;
+          if (decl.node.source && decl.node.source.value === source) {
+            if (specifiers.length === 0) {
+              decl.remove();
             }
-          });
+          }
         }
       }
     }
@@ -19,3 +35,5 @@ function stripGlimmerUtils(babel) {
 stripGlimmerUtils.baseDir = function() {
   return __dirname;
 }
+
+module.exports = stripGlimmerUtils;
